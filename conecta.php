fb5fab98c4e8f597c1c2da37baef856bb3b129ff
Kkristhian <kristhianluz@hotmail.com>
2018-04-04 19:22:22 -0300

    v1

---
+++ conecta.php
@@ -0,0 +1,10 @@
+<?php
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbname = 'loja';
+
+    $conexao = mysqli_connect($host,$user,$pass,$dbname);
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+    include_once("cabecalho.php"); 
+?>
+
+    <h1> BEM VINDO !!!</h1>
+    <img src="bebida" width="1000px" height="500px" border="0">
+
+<?php 
+    include_once("rodape.php");
+?>
